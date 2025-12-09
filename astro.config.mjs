2f27d7a4a48e23e9e3d4510056421c04f74f75d5
Alex Fuentes <alfupe@gmail.com>
2025-12-09 23:39:55 +0100

    Initial commit from Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Reemplaza con la URL de tu GitHub Pages (usuario.github.io)
+    site: 'https://alfupe.github.io',
+    // Reemplaza con el nombre exacto de tu repositorio
+    base: '/la-terreta-run-club',
+});
